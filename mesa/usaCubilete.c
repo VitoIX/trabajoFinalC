18c8804ae1824d7bfd0255b5735122c588e5f065
Victor <victorrigi@gmail.com>
2022-12-11 01:51:49 +0100

    Renombrando cubilete.c a usaCubilete.c, cubilete sera un token

---
+++ mesa/usaCubilete.c
@@ -0,0 +1,23 @@
+#ifndef inclusion_h
+#include "inclusion.h"
+#endif
+
+Dados usaCubilete(Dados tirada){
+    int indice;
+
+    for(indice = 0; indice < tirada.cantidad ; indice++){
+        tirada.numeros[indice] = aleatorio();
+    }
+    
+
+
+    return(tirada);
+}
+
+int aleatorio(){ // funcion que devuelve un número aleatorio entre 1 y TAMAÑO_DADO
+    int numero = 0; //un dado no puede valer 0 si vale 0 algo falló
+
+    numero = (rand() % (TAMANO_DADO)) + 1; //numero aleatorio entre 1 y TAMAÑO_DADO
+
+    return(numero);
+}
